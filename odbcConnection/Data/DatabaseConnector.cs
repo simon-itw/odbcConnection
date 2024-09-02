ec6f7d86ed748a59fe3263f32edb267f73996fd9
simon-itw <s.patino-lang@tn.itw-berlin.de>
2024-09-02 15:15:25 +0200

    Projektdateien hinzufügen.

---
+++ odbcConnection/Data/DatabaseConnector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+
+namespace odbcConnection.Data
+{
+    public class DatabaseConnector
+    {
+        private readonly string connectionString;
+
+        public DatabaseConnector()
+        {
+            // string aus der App.config abrufen
+            connectionString = ConfigurationManager.ConnectionStrings["VerbindungsString"].ConnectionString;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return new OdbcConnection(connectionString);
+        }
+
+        public void OpenConnection(IDbConnection connection)
+        {
+            if(connection.State == ConnectionState.Closed)
+            {
+                connection.Open();  
+            }
+        }
+
+        public void CloseConnection(IDbConnection connection)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            } 
+        }
+    }
+
+    
+    
+}
---
+++ odbcConnection/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using odbcConnection.Data;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace odbcConnection
+{
+    public partial class MainWindow : Window
+    {
+        string auswahlDatumAnfang = null;
+        string auswahlDatumEnde = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectedDateChanged_Anfang(object sender, SelectionChangedEventArgs e)
+        {
+            DatumDisplay(ref auswahlDatumAnfang,datePickerAnfang, tb_anfangDisplay);
+            BtnVisibility();
+        }
+
+        private void SelectedDateChanged_Ende(object sender, SelectionChangedEventArgs e)
+        {
+            DatumDisplay(ref auswahlDatumEnde, datePickerEnde, tb_endeDisplay);
+            BtnVisibility();
+        }
+
+        private void DatumDisplay(ref string datum, DatePicker datePicker, TextBlock tb)
+        {
+            if (datePicker.SelectedDate.HasValue)
+            {
+                datum = datePicker.SelectedDate.Value.ToString("dd.MM.yyyy");
+                tb.Text = datum;
+            }
+            else
+            {
+                datum = null;
+                tb.Text = string.Empty;
+            }
+        }
+
+        private void BtnVisibility()
+        {
+            if (!string.IsNullOrEmpty(auswahlDatumAnfang) && !string.IsNullOrEmpty(auswahlDatumEnde))
+            {
+
+                btnSend.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Send(object sender, RoutedEventArgs e)
+        {
+           
+            DatabaseConnector connect = new DatabaseConnector();
+
+            try
+            {
+                // SQLClient query string und parameter syntax
+                //string query = "INSERT INTO t_urlaub (anfangsDatum, endDatum) VALUES (@AuswahlAnfang, @AuswahlEnde)";
+
+                //Odbc query string und syntax
+                string query = "INSERT INTO t_urlaub (anfangsDatum, endDatum) VALUES (?, ?)";
+
+                using (IDbConnection connection = connect.GetConnection())
+                {
+                    connect.OpenConnection(connection);
+
+                    using(IDbCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = query;
+
+                        // parameter definieren um die eigentlichen werte zu übergeben und injections zu verhindern
+                        IDbDataParameter paramAnfang = command.CreateParameter();
+                        paramAnfang.ParameterName = "?";
+                        paramAnfang.Value = auswahlDatumAnfang;
+
+                        IDbDataParameter paramEnde = command.CreateParameter();
+                        paramEnde.ParameterName = "?";
+                        paramEnde.Value = auswahlDatumEnde; 
+
+                        command.Parameters.Add(paramAnfang);
+                        command.Parameters.Add(paramEnde);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if(rowsAffected > 0)
+                        {
+                            MessageBox.Show("Daten eingetragen");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Daten NICHT eingetragen");
+                        }  
+                    }
+
+                    connect.CloseConnection(connection);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Speichern der Daten: {ex.Message}");
+            }
+
+
+
+        }
+    }
+
+   
+}
